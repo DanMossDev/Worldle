3da2c64a2968182c86eec11f089dcdd99b39be96
Moss <danielsmoss96@gmail.com>
2022-05-07 03:16:30 +0100

    Initial commit - a fully functional if somewhat basic guess the country game

---
+++ app.js
@@ -0,0 +1,95 @@
+import { COUNTRIES } from "./countries.js";
+
+const keyboardArray = document.getElementsByClassName("keyboard-button");
+const keyboard = document.getElementById("keyboard");
+const guessString = document.getElementById("guess-string");
+const playAgain = document.getElementById("playagain");
+var userInput = document.getElementById("user-input");
+var guess = document.getElementById("guess");
+var hint = document.getElementById("hint");
+var feedback = document.getElementById("feedback");
+
+let country;
+let hintIndex = 0;
+
+userInput.addEventListener("keydown", function(e) {if (e.code === "Enter") {makeGuess()}});
+playAgain.addEventListener("click", function() {location.reload()});
+
+function randomCountry() { //Randomly selects a country - once on page launch
+    let i = Math.floor(Math.random() * COUNTRIES.length);
+    country = COUNTRIES[i];
+    console.log(country.name);
+}
+
+function eventListener() { //adds event listeners to each button in the keyboardArray array - once on page launch
+    for (let i = 0; i < keyboardArray.length; i++) {
+        keyboardArray[i].addEventListener("click", function(){buttonPress(keyboardArray[i].textContent)})
+    }
+}
+
+function updateHint() { //adds the next hint to the screen while keeping the previous hint
+    hint.innerHTML +=  `${country.hint[hintIndex]}<br><br>`;
+}
+
+function buttonPress(key) {
+    if (key === 'Enter') {
+        makeGuess();
+    }
+    else if (key === 'Space') {
+        updateInput(' ');
+    }
+    else {
+        updateInput(key);
+    }
+}
+
+function updateInput(key) {
+    if (key === 'Del') {userInput.value = userInput.value.slice( 0, userInput.value.length -1 )}
+    else {
+     userInput.value += key;
+    }
+}
+
+function makeGuess() { //compare current input to the randomly selected country
+    if (userInput.value.toLowerCase() === country.name.toLowerCase()) {
+        victorySequence();
+    }
+    else {
+        hintIndex++;
+        updateGuess();
+    }
+}
+
+function updateGuess() {
+    if (eval(guess.textContent) >= 4) { //if out of guesses, display a loss screen and remove the rest of the elements
+        guess.textContent = 'YOU LOSE - The correct answer was: ' + country.name;
+        playAgain.style.visibility = 'visible';
+        feedback.textContent = '';
+        guessString.textContent = '';
+        userInput.value = '';
+        hint.innerHTML = '';
+        keyboard.style.display = 'none';
+        userInput.style.display = 'none';
+    }
+    else { //if not, update the guess total, feedback to the player, and show new hint
+        guess.textContent = eval(guess.textContent) + 1;
+        feedback.innerHTML = `<br> ${userInput.value} isn't correct, try again!`;
+        userInput.value = '';
+        updateHint();
+    }
+}
+
+function victorySequence() {
+    guess.textContent = `YOU WIN! - It took you ${eval(guess.textContent)} tries.`;
+    playAgain.style.visibility = 'visible';
+    feedback.textContent = '';
+    guessString.textContent = '';
+    userInput.value = '';
+    hint.textContent = '';
+    keyboard.style.display = 'none';
+    userInput.style.display = 'none';
+}
+
+randomCountry();
+updateHint();
+eventListener();
---
+++ countries.js
@@ -0,0 +1,7 @@
+export const COUNTRIES = [
+    {name: 'England', hint: ['Highest Peak: Scafell Pike', 'Capital City GDP in USD: 620 Billion', 'Home to the river Aire', '<img src="./images/england_hint4.jpg">']},
+    {name: 'Scotland', hint: ['Place Name: Perth', 'Capital city is rumoured to be named for King Edwin of a neighbouring country', 'Chicken Tikka Masala was reportedly first made in this country', 'Famous Resident: Ewan McGregor']},
+    {name: 'Wales', hint: ['Famous Resident: Richard Burton', 'Place Name: Fishguard', '<img src="./images/wales_hint3.jpg">', 'Sheep-to-human ratio of 3:1']},
+    {name: 'Northern Ireland', hint: ['Political Party: Sinn Féin', 'Birthplace of the absolute temperature scale (later renamed "Kelvin")', 'Once known as Ulster', '<img src="./images/northernireland_hint4.jpg">']},
+    {name: 'Ireland', hint: ['Local Brand: Tayto', 'Filmed here: Star Wars - The Last Jedi', 'Westernmost Point: Dingle Peninsula', 'Mythological Creature: Fomorian']}
+]
